8c1445d43f625873aac1ddd8c2cf7fb1be266a4c
Carla Montano <montano.carla@gmail.com>
2022-09-13 23:32:32 -0500

    add: links and blog page

---
+++ pages/blog/[postId].tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../components/ui/Header'
+
+type Props = {}
+
+const SinglePostPage = (props: Props) => {
+  return (
+    <Header title='Single Beneficiary Story' description="Program Title" />
+  )
+}
+
+export default SinglePostPage
---
+++ pages/programs/[programsId].tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../components/ui/Header'
+
+type Props = {}
+
+const SingleProgramPage = (props: Props) => {
+  return (
+    <Header title='Our Programs' description="Program Title" />
+  )
+}
+
+export default SingleProgramPage
