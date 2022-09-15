8b4b826ce6f451cfcd02360d6a00751ea847978a
Carla Montano <montano.carla@gmail.com>
2022-09-14 23:40:54 -0500

    add: search result list

--- pages/beneficiaries/index.tsx
+++ pages/beneficiaries/index.tsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/ui/Header";
 import articles from "../../storiesData";
 
 const BlogPage = () => {
+const [searching, setSearching] = useState(false)
+const [filteredStories, setFilteredStories] = useState<any>([])
+
   const handleSearch = (e: { target: { value: string } }) => {
+    setSearching(true)
     const input = e.target.value;
 
     const filteredStory = articles.filter((article: any) =>
       article.name.toLowerCase().includes(input.toLocaleLowerCase())
     );
+
+    setFilteredStories(filteredStory)
     console.log(input, "input");
     console.log(filteredStory);
+    setSearching(false)
   };
+
+
 
   return (
     <>
@@ -27,9 +36,13 @@ const BlogPage = () => {
             onChange={(e) => handleSearch(e)}
           />
         </div>
-        {articles.map((item: any) => {
+        <div><h2>filtered stories:</h2>
+          {setFilteredStories.length > 0 && filteredStories.map((item: any) => { return (<li key={item.name}>{item.name}</li>)} )}
+        </div>
+
+        {/* {articles.map((item: any) => {
           return <p key={item.name}>{item.name}</p>;
-        })}
+        })} */}
       </main>
     </>
   );
