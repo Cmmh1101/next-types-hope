1d815be9167a70f281aa2ae1571ff0923ffe6722
Carla Montano <montano.carla@gmail.com>
2022-09-14 23:31:01 -0500

    formatter

--- pages/beneficiaries/index.tsx
+++ pages/beneficiaries/index.tsx
@@ -1,37 +1,38 @@
-import React from 'react'
-import Header from '../../components/ui/Header'
-import articles from '../../storiesData'
+import React from "react";
+import Header from "../../components/ui/Header";
+import articles from "../../storiesData";
 
 const BlogPage = () => {
+  const handleSearch = (e: { target: { value: string } }) => {
+    const input = e.target.value;
 
-  const handleSearch = (e: { target: { value: string; }; } ) => {
-    const input = e.target.value
-    
-    const filteredStory = articles.filter((article: any) => article.name.toLowerCase().includes(input.toLocaleLowerCase()))
-    console.log(input, 'input')
-    console.log(filteredStory)
-  }
+    const filteredStory = articles.filter((article: any) =>
+      article.name.toLowerCase().includes(input.toLocaleLowerCase())
+    );
+    console.log(input, "input");
+    console.log(filteredStory);
+  };
 
-  
+  return (
+    <>
+      <Header
+        title="Meet Some Of Our Beneficiaries"
+        description="We have a variety of programs that target different communities, from the little ones to the elderly. We help the needed with tools, assistance, love, adn God's word"
+      />
+      <main>
+        <div>
+          <input
+            type="search"
+            placeholder="type search"
+            onChange={(e) => handleSearch(e)}
+          />
+        </div>
+        {articles.map((item: any) => {
+          return <p key={item.name}>{item.name}</p>;
+        })}
+      </main>
+    </>
+  );
+};
 
-    return (
-      <>
-<Header title='Meet Some Of Our Beneficiaries' description="We have a variety of programs that target different communities, from the little ones to the elderly. We help the needed with tools, assistance, love, adn God's word" />
-<main>
-  <div>
-    <input type="search" placeholder="type search" onChange={(e) => handleSearch(e)} />
-    
-  </div>
-      {articles.map((item: any) => {
-        return (
-          <p key={item.name}>{item.name}</p>
-        )
-      })}
-    </main>
-      </>
-      
-    )
-  
-}
-
-export default BlogPage
+export default BlogPage;
