3f3355b59bdc5e4b62271d3ed49d756793fd15dd
Carla Montano <montano.carla@gmail.com>
2022-09-17 22:38:45 -0500

    formatting

--- components/layout/NavLinks.tsx
+++ components/layout/NavLinks.tsx
@@ -1,21 +1,54 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 interface Props {
-    className?: string
+  className?: string;
 }
 
-const NavLinks = ({className}: Props) => {
+const NavLinks = ({ className }: Props) => {
   return (
     <div className={className}>
-      <div><Link href="/"><a>Home<span></span></a></Link></div>
-      <div><Link href="/"><a>About us<span></span></a></Link></div>
-      <div><Link href="/programs"><a>Programs<span></span></a></Link></div>
-      <div><Link href="/beneficiaries"><a>Beneficiaries<span></span></a></Link></div>
-      <div><Link href="https://www.paypal.com/donate?hosted_button_id=JWBZEQHW76WC4"
-    target="_blank"><a>Donate<span></span></a></Link></div>
+      <div>
+        <Link href="/">
+          <a>
+            Home<span></span>
+          </a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/aboutUs">
+          <a>
+            About us<span></span>
+          </a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/programs">
+          <a>
+            Programs
+            <span></span>
+          </a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/beneficiaries">
+          <a>
+            Beneficiaries<span></span>
+          </a>
+        </Link>
+      </div>
+      <div>
+        <Link
+          href="https://www.paypal.com/donate?hosted_button_id=JWBZEQHW76WC4"
+          target="_blank"
+        >
+          <a>
+            Donate<span></span>
+          </a>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavLinks
+export default NavLinks;
