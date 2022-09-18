7da4ada110cbb5e381911b54a0971a8e78f39b46
Carla Montano <montano.carla@gmail.com>
2022-09-17 22:25:32 -0500

    about us structure

---
+++ pages/aboutUs/Leadership.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Leadership = (props: Props) => {
+  return (
+    <div>Leadership</div>
+  )
+}
+
+export default Leadership
---
+++ pages/aboutUs/OurPurpose.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const OurPurpose = (props: Props) => {
+  return (
+    <div>OurPurpose</div>
+  )
+}
+
+export default OurPurpose
