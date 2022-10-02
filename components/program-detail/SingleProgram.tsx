f6bf49654f31a79e6e10178c853fefe6bc59f001
Carla Montano <cm.emprendiendo@gmail.com>
2022-10-02 00:56:54 -0500

    article styles

--- components/program-detail/SingleProgram.tsx
+++ components/program-detail/SingleProgram.tsx
@@ -4,6 +4,7 @@ import { IProgramsData } from '../../interfaces/ProgramsData'
 import ArticleSections from '../ui/ArticleSection'
 import Heading from '../ui/Heading'
 import QuoteBlock from '../ui/QuoteBlock'
+import classes from './singleProgram.module.css'
 
 interface IPrograms {
   program: IProgramsData
@@ -12,21 +13,29 @@ interface IPrograms {
 const SingleProgram = ({program}: IPrograms) => {
   return (
     <ArticleSections>
-        <section>
+        <section className={classes.section}>
         <Heading title={program.title} />
-        {program.content.map((paragraph, i) => {
+        <div className={classes.article}>
+          <div className={classes.text}>
+          <div className={classes.float}>
+          <Image src={program.image} alt={program.alt} height={300} width={300} />
+          </div>
+          {program.content.map((paragraph, i) => {
             return (
               <p key={i}>{paragraph}</p>
             )
           })}
-        <div>
-        <Image src={program.image} alt={program.alt} height={400} width={400} />
-        <Image src={program.programImage1.image} alt={program.alt} height={200} width={400} />
+          </div>
         </div>
-          
+        <div className={classes.images}>
+        <Image src={program.image} alt={program.alt} height={400} width={400} />
+        <Image src={program.programImage1.image} alt={program.alt} height={400} width={400} />
+        </div>
         </section>
         
-        <QuoteBlock><p>{program.highligh}</p></QuoteBlock>
+        <QuoteBlock>
+          <p>{program.highligh}</p>
+        </QuoteBlock>
         
     </ArticleSections>
   )
