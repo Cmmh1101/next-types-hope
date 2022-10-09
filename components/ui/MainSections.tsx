4d1409e8fa514ddffc1b6f4f8b62a88ca81f1893
Carla Montano <cm.emprendiendo@gmail.com>
2022-10-09 00:05:36 -0500

    add: success page styles

--- components/ui/MainSections.tsx
+++ components/ui/MainSections.tsx
@@ -3,11 +3,12 @@ import classes from './main-section.module.css'
 
 interface Props {
     children: JSX.Element | JSX.Element[]
+    className?: string
 }
 
-const MainSections = ({children}: Props) => {
+const MainSections = ({children, className}: Props) => {
   return (
-    <main className={classes.main}>{children}</main>
+    <main className={`${classes.main} ${className}`}>{children}</main>
   )
 }
 
--- pages/Success.tsx
+++ pages/Success.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import Button from "../components/ui/Button";
+import MainSections from "../components/ui/MainSections";
+import Image from "next/image";
+import success from "../public/images/general/success.svg";
+import Heading from "../components/ui/Heading";
+import classes from '../styles/success.module.css'
 
-type Props = {}
-
-const Success = (props: Props) => {
+const Success = () => {
   return (
-    <div>Form successfully submitted!</div>
-  )
-}
+      <MainSections className={classes.section}>
+        <Heading title="Form successfully submitted!" />
+        {/* <Link href="/">
+          <a>Go To Home Page</a>
+        </Link> */}
+        <Button link="/" text="Go To Home Page" />
+        <div>
+        <Image
+          src={success}
+          alt="success mail sent"
+          width={300}
+          height={300}
+          layout="responsive"
+        />
+        </div>
+       
+      </MainSections>
+  );
+};
 
-export default Success
+export default Success;
