44acb4dabeee3ec1c495fe3104fc949152df3e20
Carla Montano <cm.emprendiendo@gmail.com>
2022-10-09 23:51:28 -0500

    fix: footer responsive logo

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,28 +1,38 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import React from 'react'
-import classes from './footer.module.css'
-import logo from '/public/logo.png'
-import NavLinks from './NavLinks'
-import SocialIcons from '../ui/SocialIcons'
-import DonateBtn from '../ui/DonateBtn'
-
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import classes from "./footer.module.css";
+import logo from "/public/logo.png";
+import NavLinks from "./NavLinks";
+import SocialIcons from "../ui/SocialIcons";
+import DonateBtn from "../ui/DonateBtn";
 
 const Footer = () => {
   return (
     <footer className={`container ${classes.footer}`}>
-      <NavLinks className={classes.navigation} donateLink={classes.donateLink} />
+      <NavLinks
+        className={classes.navigation}
+        donateLink={classes.donateLink}
+      />
       <div className={classes.section}>
-      <Link href="/" className={classes.logo}>
-        <a>
-        <Image src={logo} alt="logo" height={200} width={200} />
-        </a>
-      </Link>
-      <DonateBtn className={classes.donate} />
-      <SocialIcons className={classes.socials} />
+        <div className={classes.logo}>
+          <Link href="/">
+            <a>
+              <Image
+                src={logo}
+                alt="logo"
+                height={200}
+                width={200}
+                layout="responsive"
+              />
+            </a>
+          </Link>
+        </div>
+        <DonateBtn className={classes.donate} />
+        <SocialIcons className={classes.socials} />
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
