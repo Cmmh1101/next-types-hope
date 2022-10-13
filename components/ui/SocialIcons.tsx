b5120f1bb825fc45e5590f32c8fb710c5da67524
Carla Montano <cm.emprendiendo@gmail.com>
2022-10-13 00:31:37 -0500

    add: aria to icons

--- components/ui/SocialIcons.tsx
+++ components/ui/SocialIcons.tsx
@@ -13,13 +13,13 @@ const SocialIcons = ({className}: Props) => {
   return (
     <div className={classes.icons}>
       <a href='https://facebook.com' aria-role="link" aria-label="facebook link" className={classes.fb} target="_blank" rel="noreferrer">
-        <SiFacebook />
+        <SiFacebook aria-label="facebook link" />
       </a>
       <a href='https://instagram.com' aria-role="link" aria-label="instagram link" className={classes.in} target="_blank" rel="noreferrer">
-        <TiSocialInstagramCircular />
+        <TiSocialInstagramCircular aria-label="instagram link" />
       </a>
       <a href='https://youtube.com' aria-role="link" aria-label="youtube link" className={classes.yt} target="_blank" rel="noreferrer">
-        <SiYoutubemusic />
+        <SiYoutubemusic aria-label="youtube link" />
       </a>
     </div>
     
