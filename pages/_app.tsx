4018135a4e866b90a27bcb99046934b964f09b17
Carla Montano <cm.emprendiendo@gmail.com>
2022-11-08 20:55:35 -0600

    google analytics tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,48 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Layout from '../components/layout/Layout'
-import Head from 'next/head'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/layout/Layout";
+import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Head>
-        <title>Hope for the nations</title>
-        <meta name="description" content="Non profit hope for the nations helping the needed with love, hope, and God's word" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      {/* <script async strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} /> */}
+      <script
+        id="google-analytics"
+        // strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}, {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+      <Layout>
+        <Head>
+          <title>Hope for the nations</title>
+          <meta
+            name="description"
+            content="Non profit hope for the nations helping the needed with love, hope, and God's word"
+          />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
-export default MyApp
-
-
+export default MyApp;
