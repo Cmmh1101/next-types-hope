937501ba997b69b649defd03b3045f914fcd6b58
Carla Montano <cm.emprendiendo@gmail.com>
2022-12-28 00:01:22 -0600

    fix btn donate

--- components/ui/DonateBtn.tsx
+++ components/ui/DonateBtn.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import classes from './donateBtn.module.css'
+import classes from "./donateBtn.module.css";
 
 interface Props {
   className?: string;
@@ -7,11 +7,16 @@ interface Props {
 
 const DonateBtn = ({ className }: Props) => {
   return (
-    <div className={`${classes.btn} ${className}`} role="button">
-        <a href="https://www.paypal.com/donate?hosted_button_id=JWBZEQHW76WC4"
-        target="_blank" rel="noreferrer" role="button" aria-label="donate button">
-          Donate
-        </a>
+    <div className={classes.btn} role="button">
+      <a
+        href="https://www.paypal.com/donate?hosted_button_id=JWBZEQHW76WC4"
+        target="_blank"
+        rel="noreferrer"
+        role="button"
+        aria-label="donate button"
+      >
+        Donate
+      </a>
     </div>
   );
 };
