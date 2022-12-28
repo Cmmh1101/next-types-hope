f33a9078e442b5a2a3310de89e0d8b8a9df2e843
Carla Montano <cm.emprendiendo@gmail.com>
2022-12-27 23:53:05 -0600

    change donate btn styles

--- components/ui/Heading.tsx
+++ components/ui/Heading.tsx
@@ -12,7 +12,11 @@ const Heading = ({ title, subtitle, decoText }: Props) => {
     <div className={classes.heading}>
       <h2>{title}</h2>
       {title && <div className={classes.wavy} />}
-      {subtitle && <h3 className="wavy">{subtitle}</h3>}
+      {subtitle && (
+        <>
+          <h3 className="wavy">{subtitle}</h3> <div className={classes.wavy} />
+        </>
+      )}
       {decoText && <span>{decoText}</span>}
     </div>
   );
